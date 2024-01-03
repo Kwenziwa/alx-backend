8166495eb14f7cac79e01a9cd0ccd71c3d2c5669
kwenziwa <kwenziwa@live.com>
2024-01-03 15:27:39 -0800

    task 1

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,54 @@
+Certainly! Here's a revised version of your comment sections:
+
+```python
+#!/usr/bin/python3
+""" FIFO Caching Implementation """
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ Class that inherits from BaseCaching and implements a caching system
+        using the First-In-First-Out (FIFO) algorithm. """
+
+    def __init__(self):
+        super().__init__()
+        # Dictionary to store data and pointers for FIFO algorithm
+        self.data = {}
+        self.next_in, self.next_out = 0, 0
+
+    def _pop(self):
+        """ FIFO algorithm: Remove the oldest element from the cache """
+        self.next_out += 1
+        key = self.data[self.next_out]
+        del self.data[self.next_out], self.cache_data[key]
+
+    def _push(self, key, item):
+        """ FIFO algorithm: Add a new element to the cache """
+        # Check if the cache is at its maximum capacity
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS - 1:
+            print("DISCARD: {}".format(self.data[self.next_out + 1]))
+            # Remove the oldest element if the cache is full
+            self._pop()
+        # Add the new element to the cache and update pointers
+        self.cache_data[key] = item
+        self.next_in += 1
+        self.data[self.next_in] = key
+
+    def put(self, key, item):
+        """ Add or update an item in the cache using the FIFO algorithm """
+        if key and item:
+            if key in self.cache_data:
+                # If the key already exists, update the item
+                self.cache_data[key] = item
+            else:
+                # If the key is new, add the item using the FIFO algorithm
+                self._push(key, item)
+
+    def get(self, key):
+        """ Retrieve the value associated with the provided key """
+        if key is None or self.cache_data.get(key) is None:
+            return None
+        if key in self.cache_data:
+            # If the key exists, return the associated value
+            return self.cache_data[key]
